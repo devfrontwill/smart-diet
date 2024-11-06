0bd2c392e7f553a75b7b7fab062b72ce224cdd09
Wiliam Roza <devfrontwill@gmail.com>
2024-11-05 23:22:07 -0300

    initial commit

---
+++ src/app/step/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import { colors } from '../../../constants/color';
+
+export default function Step(){
+    return(
+        <View>
+            <Text>Formulario 1</Text>
+        </View>
+    )
+}

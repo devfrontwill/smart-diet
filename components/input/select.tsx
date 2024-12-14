6bec268b8ee9a161c4d6a19b4b2f3f7780683a2b
Wiliam Roza <devfrontwill@gmail.com>
2024-12-14 16:26:07 -0300

    Feat: Criado um modal para exibir as options ao selecionar o genero e feito toda configuração, estilização

--- components/input/select.tsx
+++ components/input/select.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList, Modal } from 'react-native';
 import { Controller } from 'react-hook-form';
 import { colors } from '@/constants/color';
@@ -17,6 +18,9 @@ interface SelectProps {
 }
 
 export function Select({ name, control, placeholder, error, options }: SelectProps) {
+
+    const [visible, setVisible] = useState(false);
+
     return (
         <View style={styles.container}>
             <Controller
@@ -25,10 +29,43 @@ export function Select({ name, control, placeholder, error, options }: SelectPro
 
                 render={({ field: { onChange, onBlur, value } }) => (
                     <>
-                        <TouchableOpacity style={styles.select}>
-                            <Text>Selecione algo...</Text>
+                        <TouchableOpacity style={styles.select} onPress={() => setVisible(true)} >
+                            <Text>
+                                {value ? options.find(option => option.value === value)?.label : placeholder}
+                            </Text>
                             <Feather name='arrow-down' size={16} color="#000" />
                         </TouchableOpacity>
+
+                        <Modal
+                            visible={visible}
+                            animationType='fade'
+                            transparent={true}
+                            onRequestClose={() => setVisible(false)}
+                        >
+
+                            <TouchableOpacity
+                                style={styles.modalContainer}
+                                activeOpacity={1}
+                                onPress={() => setVisible(false)}
+                            >
+                                <TouchableOpacity style={styles.modalContent} activeOpacity={1}>
+                                    <FlatList
+                                        contentContainerStyle={{ gap: 4 }}
+                                        data={options}
+                                        keyExtractor={(item) => item.value.toString()}
+                                        renderItem={({ item }) => (
+                                            <TouchableOpacity style={styles.option} onPress={() => {
+                                                onChange(item.value)
+                                                setVisible(false)
+                                            }}>
+                                                <Text>{item.label}</Text>
+                                            </TouchableOpacity>
+                                        )}
+                                    />
+                                </TouchableOpacity>
+                            </TouchableOpacity>
+
+                        </Modal>
                     </>
                 )}
             />
@@ -60,5 +97,22 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         paddingHorizontal: 10,
         borderRadius: 4,
+    },
+    modalContainer: {
+        backgroundColor: 'rgba(0,0,0, 0.5)',
+        flex: 1,
+        justifyContent: 'center'
+    },
+    modalContent: {
+        backgroundColor: colors.white,
+        marginHorizontal: 10,
+        borderRadius: 8,
+        padding: 20
+    },
+    option: {
+        paddingVertical: 14,
+        backgroundColor: 'rgba(208, 208, 208, 0.40)',
+        borderRadius: 4,
+        paddingHorizontal: 8,
     }
 })

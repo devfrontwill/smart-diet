bd502e67b5b53d6e401886bfb80500f25484b27e
Wiliam Roza <devfrontwill@gmail.com>
2024-12-16 15:10:01 -0300

    Feat: Terminado o formulario com selects criado do zero da segunda pagina, reutilizado o botao da primeira pagina.

--- src/app/create/index.tsx
+++ src/app/create/index.tsx
@@ -6,10 +6,11 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Header } from '@/components/header';
 import { Select } from '@/components/input/select';
+import { useDataStore } from '@/store/data';
 
 const schema = z.object({
     gender: z.string().min(1, { message: "O genero é obrigatório" }),
-    objective: z.string().min(1, { message: "A objetivo é obrigatória" }),    
+    objective: z.string().min(1, { message: "A objetivo é obrigatória" }),
     level: z.string().min(1, { message: "Selecione seu nivel" }),
 })
 
@@ -21,10 +22,35 @@ export default function Create() {
         resolver: zodResolver(schema)
     })
 
+    const setPageTwo = useDataStore(state => state.setPageTwo)
+
     const genderOptions = [
         { label: "Masculino", value: "masculino" },
         { label: "Feminino", value: "feminino" }
     ]
+
+    const levelOptions = [
+        { label: 'Sedentário (pouco ou nenhuma atividade fisica)', value: 'Sedentario' },
+        { label: 'Levemente Ativo (exercicios 1 a 3 vezes por semana)', value: 'Levemene Ativo (exercicios 1 a 3 vezes na semana)' },
+        { label: 'Moderadamente Ativo (exercicios 3 a 5 vezes por semana)', value: 'Moderadamente Ativo (exercicios 3 a 5 vezes na semana)' },
+        { label: 'Altamente Ativo (exercicios 5 a 7 vezes por semana)', value: 'Altamente Ativo (exercicios 5 a 7 vezes por semana)' },
+    ]
+
+    const objectiveOptions = [
+        { label: 'Emagrecer', value: 'Emagrecer' },
+        { label: 'Hipertrofia', value: 'Hipertrofia' },
+        { label: 'Hipertrofia + Definição', value: 'Hipertrofia + Definição' },
+        { label: 'Definição', value: 'Definição' },
+    ]
+
+    function handleCreate(data:FormData){
+        setPageTwo({
+            level: data.level,
+            gender: data.gender,
+            objective: data.objective
+        })
+        console.log("Navegando para a ultima pagina");
+    }
 
     return (
         <View style={styles.container}>
@@ -42,13 +68,35 @@ export default function Create() {
                     error={errors.gender?.message}
                     options={genderOptions}
                 />
+
+                <Text style={styles.label}>Selecione o nivel de atividade fisica:</Text>
+                <Select
+                    control={control}
+                    name='level'
+                    placeholder='Selecione seu nivel de atividade fisica: '
+                    error={errors.level?.message}
+                    options={levelOptions}
+                />
+
+                <Text style={styles.label}>Selecione o seu objetivo:</Text>
+                <Select
+                    control={control}
+                    name='objective'
+                    placeholder='Selecione o seu objetivo: '
+                    error={errors.objective?.message}
+                    options={objectiveOptions}
+                />
+
+                <Pressable style={styles.button} onPress={handleSubmit(handleCreate)}>
+                    <Text style={styles.buttonText}> Avançar </Text>
+                </Pressable>
             </ScrollView>
         </View>
     );
 }
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         backgroundColor: colors.background,
     },
     label: {
@@ -60,5 +108,17 @@ const styles = StyleSheet.create({
     content: {
         paddingLeft: 16,
         paddingRight: 16,
+    },
+    button: {
+        backgroundColor: colors.blue,
+        height: 44,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 })

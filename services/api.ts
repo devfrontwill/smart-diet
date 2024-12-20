0bf69a47dad42e5d7ce11ebc276cd231acc87e44
Wiliam Roza <devfrontwill@gmail.com>
2024-12-20 10:09:06 -0300

    Feat: Substituida a API que estava sendo utilizada para desenvolvimento pela já feita o deploy .

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// |baseURL|http://192.168.1.6:3333|baseURL| /create
-
 export const api = axios.create({
-    baseURL: "http://192.168.1.6:3333"
+    baseURL: "https://dieta-ai-backend.vercel.app/"
 })
